e4e6645e3ed7b64747f736a87f77ad8bd948aa48
Ivan <iv.lyutov@gmail.com>
2017-03-16 23:24:55 +0300

    address line1-3

--- lib/avalara/request/address.rb
+++ lib/avalara/request/address.rb
@@ -3,9 +3,9 @@
 module Avalara
   module Request
     class Address < Avalara::Types::Stash
-      property :line1, :from => :line_1
-      property :line2, :from => :line_2
-      property :line3, :from => :line_3
+      property :line1
+      property :line2
+      property :line3
       property :city
       property :region
       property :country
